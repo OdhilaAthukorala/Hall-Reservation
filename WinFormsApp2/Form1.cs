e2e80773c2cb96b9328ca56a1ca3de2c2b6fbb0e
HP <HP@DESKTOP-AI7M194>
2024-07-19 18:05:22 +0530

    Add project files.

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,40 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Add your code here
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            // Add your code here
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            // Add your code here
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // Add your code here
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Add your code here
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Add your code here
+        }
+    }
+}
